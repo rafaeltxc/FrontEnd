f28e14722a9355f8cf01a26d6b256c79f8d22545
Rafael Costa <rafael.txct@gmail.com>
2022-10-18 15:05:00 -0300

    finishing project, stopwatch functions working

--- js/timer.js
+++ js/timer.js
@@ -1,3 +1,4 @@
+//Timer
 const numbers = document.querySelectorAll('[data-number]');
 const deletion = document.getElementById('delete');
 const begin = document.getElementById('play');
@@ -52,7 +53,7 @@ begin.addEventListener('click', () => {
         if(second.value > 0) {
             second.value -= 1;
         } else if(second.value == 0 && minute.value > 0) {
-            second.value = 60;
+            second.value = 59;
             minute.value -= 1;
         } else if(hour.value > 0) {
             minute.value = 60;
@@ -67,3 +68,71 @@ begin.addEventListener('click', () => {
     
 })
 
+//Stopwatch
+const play = document.getElementById('play-stopwatch');
+const refresh = document.getElementById('refresh');
+const clock = document.getElementById('clock');
+const imgPlay = document.getElementById('img-play-stopwatch');
+const imgRefresh = document.getElementById('img-refresh');
+const imgClock = document.getElementById('img-clock');
+const history = document.getElementById('history');
+
+const stopMilli = document.getElementById('stop_milli');
+const stopSecond = document.getElementById('stop_second');
+const stopMinute = document.getElementById('stop_minute');
+const stopHour = document.getElementById('stop_hour');
+
+play.addEventListener('click', () => {
+    refresh.className = 'refresh-show';
+    clock.className = 'clock-show';
+    play.className = 'play-stopwatch-hide';
+    imgRefresh.className = 'img-refresh';
+    imgClock.className ='img-clock';
+    imgPlay.className = 'img-play-stopwatch-hide';
+
+    stopHour.innerText = 0;
+    stopMinute.innerText = 0;
+    stopSecond.innerText = 0;
+    stopMilli.innerText = 0;
+
+    const stopwatch = setInterval(() => {
+        stopMilli.innerText = parseInt(stopMilli.innerText) + 1;
+        if(stopMilli.innerText == 100) {
+            stopMilli.innerText = 0;
+            stopSecond.innerText = parseInt(stopSecond.innerText) + 1;
+        } else if(stopSecond.innerText == 60) {
+            stopSecond.innerText = 0;
+            stopMinute.innerText = parseInt(stopMinute.innerText) + 1;
+        } else if(stopMinute.innerText == 60) {
+            stopMinute.innerText = 0;
+            stopMinute.innerText = parseInt(stopMinute.innerText) + 1;
+        } else if(stopHour.innerText == 60) {
+            stopHour.innerText = 0;
+            stopHour.innerText = parseInt(stopHour.innerText) + 1;
+        }
+
+    }, 10)
+
+    refresh.addEventListener('click', () => {
+        clearInterval(stopwatch);
+
+        refresh.className = 'refresh';
+        clock.className = 'clock';
+        play.className = 'play-stopwatch';
+        imgRefresh.className = 'img-refresh-hide';
+        imgClock.className ='img-clock-hide';
+        imgPlay.className = 'img-play-stopwatch';
+
+        stopHour.innerText = '';
+        stopMinute.innerText = '';
+        stopSecond.innerText = '';
+        stopMilli.innerText = '';
+        history.innerText = '';
+    })
+
+    const paragraph = document.createElement('p');
+    clock.addEventListener('click', () => {
+        paragraph.innerText += `${stopHour.innerText}:${stopMinute.innerText}:${stopSecond.innerText}:${stopMilli.innerText}\n`;
+        history.append(paragraph)
+    })
+})
